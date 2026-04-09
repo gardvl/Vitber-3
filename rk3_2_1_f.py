c7dcb58882122531374bfa60aa84896f513b18ae
knutLeif <knuthste@stud.ntnu.no>
2026-04-09 15:31:42 +0200

    Begynner på 1f

--- rk3_2_1_f.py
+++ rk3_2_1_f.py
@@ -4,5 +4,40 @@ import matplotlib.pyplot as plt
 import rk3_2_1c as opg1c
 
 
-def guess_b(b_0, b_1, b_tol, x_span, y_n, h_0, tol, f, alpha):
-    pass
+#Parameters
+y_0 = np.array([0,2])
+tol = 10e-7
+alpha = 0.8
+x_span = [0 ,2*np.pi]
+h_0 = 0.1
+
+def secant_RK(RK,b_0,b_1,b_tol, x_span, h_0, tol, alpha):
+    x_list_list = []
+    y_list_list = []
+    h_list_list = []
+
+    x_vals, y_vals, h_vals = RK(x_span, np.array([0,b_0]), h_0, tol, opg1c.f, alpha)
+    x_list_list.append(x_vals)
+    y_list_list.append(y_vals)
+    h_list_list.append(h_vals)
+
+    x_vals, y_vals, h_vals = RK(x_span, np.array([0,b_1]), h_0, tol, opg1c.f, alpha)
+    x_list_list.append(x_vals)
+    y_list_list.append(y_vals)
+    h_list_list.append(h_vals)
+    
+
+    while abs(b_1-b_0) > b_tol:
+
+        b_2 = (b_0*y_list_list[-1][-1] - b_1*y_list_list[-2][-1]) / (y_list_list[-1][-1] - y_list_list[-2][-1])
+
+        b_0 = b_1
+        b_1 = b_2
+
+        x_vals, y_vals, h_vals = RK(x_span, np.array([0,b_1]), h_0, tol, opg1c.f, alpha)
+        x_list_list.append(x_vals)
+        y_list_list.append(y_vals)
+        h_list_list.append(h_vals)
+    return x_list_list, y_list_list, h_list_list
+
+
