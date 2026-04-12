77d836e9c2c6b6e58ae1529f1e781051762f0d71
gardvl <gardvl@stud.ntnu.no>
2026-04-12 11:22:20 +0200

    2a-c

--- opg_2a.py
+++ opg_2a.py
@@ -11,8 +11,25 @@ def Matrix_to_vec(M):
     return np.array(m)
 
 def vec_to_Matrix(m):
-    M = np.zeros((4,4), dtype=complex)
-    for i in range(4):
-        for j in range(4):
-            M[i,j] = m[i*4+j]
+    M = np.zeros((2,2), dtype=complex)
+    k = 0
+    for i in range(2):
+        for j in range(2):
+            M[i,j] = m[k]
+            k += 1
+    k = 0
+    for i in range(2):
+        for j in range(2):
+            M[i,j] += 1j * m[4 + k]
+            k += 1
+
     return M
+'''
+
+M = np.array([[1+2j, 3+4j],
+              [5+6j, 7+8j]])
+
+m = Matrix_to_vec(M)
+M_back = vec_to_Matrix(m)
+print(np.allclose(M, M_back))
+'''
--- opg_2c.py
+++ opg_2c.py
@@ -17,3 +17,25 @@ def one_vec_to_four_M(v):
     M4 = opg_2a.vec_to_Matrix(v4)
     return M1, M2, M3, M4
 
+'''
+M1 = np.array([[1+2j, 3+4j],
+               [5+6j, 7+8j]])
+
+M2 = np.array([[2+1j, 4+3j],
+               [6+5j, 8+7j]])
+
+M3 = np.array([[1+1j, 2+2j],
+               [3+3j, 4+4j]])
+
+M4 = np.array([[5+0j, 0+5j],
+               [1+2j, 2+1j]])
+
+v = four_M_to_one_vec(M1, M2, M3, M4)
+M1_, M2_, M3_, M4_ = one_vec_to_four_M(v)
+
+print("M1:", np.allclose(M1, M1_))
+print("M2:", np.allclose(M2, M2_))
+print("M3:", np.allclose(M3, M3_))
+print("M4:", np.allclose(M4, M4_))
+'''
+
