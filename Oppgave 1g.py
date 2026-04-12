2ec247857056716a932dc2176e177c36f9f69eb9
knutLeif <knuthste@stud.ntnu.no>
2026-04-12 11:10:59 +0200

    1g ferdig

---
+++ Oppgave 1g.py
@@ -0,0 +1,25 @@
+#Oppgave 1g
+import numpy as np
+import matplotlib.pyplot as plt
+import rk3_2_1_f as opg1f
+import rk3_2_1c as opg1c
+
+x_span = [0,12]
+h_0 = 0.1
+tol = 1e-7
+alpha = 0.8
+
+def g(x,y):
+    return np.array([y[1], y[0]+np.sin(x)])
+
+
+x_list_list, y_list_list, h_list_list = opg1f.secant_RK(opg1c.rk3_2,-3,4,1e-7, x_span, h_0, tol,g, alpha)
+
+y_vals = y_list_list[-1]
+y_vals = list(np.array(y_vals)[:, 0])
+plt.plot(x_list_list[-1], y_vals)
+plt.grid()
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Boundary value problem")
+plt.show()
--- rk3_2_1_f.py
+++ rk3_2_1_f.py
@@ -10,45 +10,47 @@ alpha = 0.8
 x_span = [0 ,2*np.pi]
 h_0 = 0.1
 
-def secant_RK(RK,b_0,b_1,b_tol, x_span, h_0, tol, alpha):
+def secant_RK(RK,b_0,b_1,b_tol, x_span, h_0, tol, f, alpha):
     x_list_list = []
     y_list_list = []
     h_list_list = []
 
-    x_vals, y_vals, h_vals = RK(x_span, np.array([0,b_0]), h_0, tol, opg1c.f, alpha)
+    x_vals, y_vals, h_vals = RK(x_span, np.array([0,b_0]), h_0, tol, f, alpha)
     x_list_list.append(x_vals)
     y_list_list.append(y_vals)
     h_list_list.append(h_vals)
 
-    x_vals, y_vals, h_vals = RK(x_span, np.array([0,b_1]), h_0, tol, opg1c.f, alpha)
+    x_vals, y_vals, h_vals = RK(x_span, np.array([0,b_1]), h_0, tol, f, alpha)
     x_list_list.append(x_vals)
     y_list_list.append(y_vals)
     h_list_list.append(h_vals)
     
 
-    while abs(b_1-b_0) > b_tol:
+    while abs(y_list_list[-1][-1][0]) > b_tol:
 
         b_2 = (b_0*y_list_list[-1][-1][0] - b_1*y_list_list[-2][-1][0]) / (y_list_list[-1][-1][0] - y_list_list[-2][-1][0])
 
         b_0 = b_1
         b_1 = b_2
 
-        x_vals, y_vals, h_vals = RK(x_span, np.array([0,b_1]), h_0, tol, opg1c.f, alpha)
+        x_vals, y_vals, h_vals = RK(x_span, np.array([0,b_1]), h_0, tol, f, alpha)
         x_list_list.append(x_vals)
         y_list_list.append(y_vals)
         h_list_list.append(h_vals)
     
     return x_list_list, y_list_list, h_list_list
 
-
-x_list_list, y_list_list, h_list_list = secant_RK(opg1c.rk3_2,-3,3,1e-7, x_span, h_0, tol, alpha)
+'''
+x_list_list, y_list_list, h_list_list = secant_RK(opg1c.rk3_2,-3,4,1e-7, x_span, h_0, tol,opg1c.f, alpha)
 
 fig1 = plt.figure(figsize=(12,6))
 for i in range(len(x_list_list)):
-    y_vals = [y_vec[0] for y_vec in y_list_list[i]]
+    # y_vals = [y_vec[0] for y_vec in y_list_list[i]]
+    y_vals = list(np.array(y_list_list[i])[:, 0])
     plt.plot(x_list_list[i], y_vals, label = f"Iterasjon {i}")
 plt.legend()
 plt.grid()
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
+'''
