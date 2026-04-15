38d33b3dff0579944f6f10e3df47a2de7fc2ef98
gardvl <gardvl@stud.ntnu.no>
2026-04-15 12:13:25 +0200

    start 2g 2

--- opg_2g.py
+++ opg_2g.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import opg_2e as opg2e
 import opg_2f as opg2f
 
 l = 1
@@ -7,3 +8,10 @@ m = 101
 
 x = np.linspace(0,l,m)
 y = np.zeros((32,m))
+
+eps = [0,1,2]
+
+solutions = []
+
+for i in eps:
+    solve = sp.solve_bvp(opg_2e.better_function,)
