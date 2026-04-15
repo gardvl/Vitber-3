6ac6d2d935d34a95f08cc0fef818a7d171b1b9b1
knutLeif <knuthste@stud.ntnu.no>
2026-04-15 13:24:44 +0200

    Made the density of states

--- Opg_2h.py
+++ Opg_2h.py
@@ -1,8 +1,8 @@
 #Oppgave 2h
-
-
 import numpy as np
 import matplotlib.pyplot as plt
+import opg_2c
+
 
 def make_4_by_4_matrix(m1, m2, m3, m4):
     return np.block([[m1, m2], [m3, m4]])
@@ -14,4 +14,22 @@ def make_p_hat():
     return make_4_by_4_matrix(ident, zero, zero,  -1*ident)
 
 
-def make_D_over_D_0(x, epsilon, gamma, gamma_thilde, ):
+def make_D_over_D_0(x_vec, epsilons, solutions):
+    densities = []
+    for epsilon, solution in zip(epsilons, solutions):
+        gamma, gamma_thilde, omega, omega_thilde = opg_2c.one_vec_to_four_M(solution.y) 
+        
+        N = np.zeros((x_vec.size, 2,2), dtype= complex)
+        N_thilde = N
+        g_hat = np.zeros((x_vec.size, 4,4), dtype= complex)
+        density = []
+        for i, x in enumerate(x_vec):
+            N[i] = np.invert(np.identity(2, dtype= complex) - gamma[i] @ gamma_thilde[i])
+            N_thilde[i] = np.invert(np.identity(2, dtype= complex) - gamma_thilde[i] @ gamma[i])
+            g_hat[i] = make_4_by_4_matrix(2*N[i]-np.identity(2, dtype=complex), 2*N[i]@gamma[i], -2*N_thilde[i]@gamma_thilde[i], -2*N_thilde[i] + np.identity(2, dtype=complex))
+            density.append(np.trace(make_p_hat() @ g_hat[i]).real/4)
+        
+        densities.append(np.array(density))
+    return np.array(densities)
+
+
