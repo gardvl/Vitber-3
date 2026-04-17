0c1d35a89136e9705cab63f8dbd22e06a571c636
knutLeif <knuthste@stud.ntnu.no>
2026-04-17 12:38:16 +0200

    Fortsettelse på 2i

--- opg_2i.py
+++ opg_2i.py
@@ -1,6 +1,8 @@
 #Oppgave 2i
 import numpy as np
 import matplotlib.pyplot as plt
+import opg_2g
+from Opg_2h import make_D_over_D_0
 
 zeta = 3
 
@@ -36,3 +38,16 @@ def make_gamma_right(epsilon):
 
 def make_gamma_thilde_right(epsilon):
     return np.array([[0, make_s_minus(epsilon)/(1+make_c_minus(epsilon))], [make_s_plus(epsilon)/(1+make_c_plus(epsilon)), 0]], dtype=complex)*np.exp(-1j*theta_R)
+
+
+l = 1
+m = 101
+eps_list = [0,1,2]
+gamma_left = make_gamma_left()
+solutions = opg_2g.usadel_solution(eps_list,l,m, gamma_left, gamma_thilde_left, gamma_right, gamma_thilde_right, zeta)
+
+x_vec = np.linspace(0, l, m)
+
+densities = make_D_over_D_0(x_vec, eps_list, solutions)
+
+
