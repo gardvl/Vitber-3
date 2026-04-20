9b13821ea989417b1d6a69dcbf4a3d2518bd0eb7
knutLeif <knuthste@stud.ntnu.no>
2026-04-20 14:51:09 +0200

    ENDELIG!! 2i ferdig

--- opg_2f.py
+++ opg_2f.py
@@ -15,8 +15,8 @@ def usadel_boundary(v_left, v_right, epsilon, gamma_left, gamma_thilde_left, gam
     m1 = omega_0 + 1/(l*zeta)*(np.identity(2, dtype=complex)-gamma_0 @ gamma_thilde_left(epsilon)) @ N_L @ (gamma_left(epsilon) - gamma_0)
     m2 = omega_thilde_0 + 1/(l*zeta)*(np.identity(2, dtype=complex)-gamma_thilde_0 @ gamma_left(epsilon)) @ N_thilde_L @ (gamma_thilde_left(epsilon) - gamma_thilde_0)
 
-    m3 = omega_l + 1/(l*zeta)*(np.identity(2, dtype=complex)-gamma_l @ gamma_thilde_right(epsilon)) @ N_R @ (gamma_right(epsilon) - gamma_l)
-    m4 = omega_thilde_l + 1/(l*zeta)*(np.identity(2, dtype=complex)-gamma_thilde_l @ gamma_right(epsilon)) @ N_thilde_R @ (gamma_thilde_right(epsilon) - gamma_thilde_l)
+    m3 = omega_l - 1/(l*zeta)*(np.identity(2, dtype=complex)-gamma_l @ gamma_thilde_right(epsilon)) @ N_R @ (gamma_right(epsilon) - gamma_l)
+    m4 = omega_thilde_l - 1/(l*zeta)*(np.identity(2, dtype=complex)-gamma_thilde_l @ gamma_right(epsilon)) @ N_thilde_R @ (gamma_thilde_right(epsilon) - gamma_thilde_l)
 
     return opg2c.four_M_to_one_vec(m1, m2, m3, m4)
 
