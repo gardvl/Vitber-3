41e3826750023e3e3ba1acf7e3c4e8cfd2c9f160
knutLeif <knuthste@stud.ntnu.no>
2026-04-20 12:16:12 +0200

    Triksing of fiksing (2i progress)

--- opg_2i.py
+++ opg_2i.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 import opg_2g
 from Opg_2h import make_D_over_D_0
 
-zeta = 3
+delta = 0.01
 
 def make_v_minus(epsilon):
-    return np.arctan(-1/(epsilon + 1j*zeta))
+    return np.arctan(-1/(epsilon + 1j*delta))
 
 def make_v_plus(epsilon):
-    return np.arctan(1/(epsilon + 1j*zeta))
+    return np.arctan(1/(epsilon + 1j*delta))
 
 def make_c_minus(epsilon):
     return np.cosh(make_v_minus(epsilon))
@@ -24,19 +24,17 @@ def make_s_minus(epsilon):
 def make_s_plus(epsilon):
     return np.sinh(make_v_plus(epsilon))
 
-theta_L = 0
-theta_R = 0
 
-def make_gamma_left(epsilon):
+def make_gamma_left(epsilon, theta_L):
     return np.array([[0, make_s_plus(epsilon)/(1+make_c_plus(epsilon))], [make_s_minus(epsilon)/(1+make_c_minus(epsilon)), 0]], dtype=complex)*np.exp(1j*theta_L)
 
-def make_gamma_thilde_left(epsilon):
+def make_gamma_thilde_left(epsilon, theta_L):
     return np.array([[0, make_s_minus(epsilon)/(1+make_c_minus(epsilon))], [make_s_plus(epsilon)/(1+make_c_plus(epsilon)), 0]], dtype=complex)*np.exp(-1j*theta_L)
 
-def make_gamma_right(epsilon):
+def make_gamma_right(epsilon, theta_R):
     return np.array([[0, make_s_plus(epsilon)/(1+make_c_plus(epsilon))], [make_s_minus(epsilon)/(1+make_c_minus(epsilon)), 0]], dtype=complex)*np.exp(1j*theta_R)
 
-def make_gamma_thilde_right(epsilon):
+def make_gamma_thilde_right(epsilon, theta_R):
     return np.array([[0, make_s_minus(epsilon)/(1+make_c_minus(epsilon))], [make_s_plus(epsilon)/(1+make_c_plus(epsilon)), 0]], dtype=complex)*np.exp(-1j*theta_R)
 
 
@@ -44,7 +42,7 @@ l = 1
 m = 101
 eps_list = [0,1,2]
 gamma_left = make_gamma_left()
-solutions = opg_2g.usadel_solution(eps_list,l,m, gamma_left, gamma_thilde_left, gamma_right, gamma_thilde_right, zeta)
+solutions = opg_2g.usadel_solution(eps_list,l,m, gamma_left, gamma_thilde_left, gamma_right, gamma_thilde_right, delta)
 
 x_vec = np.linspace(0, l, m)
 
